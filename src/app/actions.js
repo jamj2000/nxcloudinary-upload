098b98ce0ee52dd50fc1bfdbdfb5189edf579105
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-12-07 22:45:22 +0100

    CLOUDINARY_URL

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,15 +1,14 @@
 'use server'
 import { v2 as cloudinary } from 'cloudinary';
-import path from 'node:path'
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET
+// });
 
 
+cloudinary.config(process.env.CLOUDINARY_URL || '')
 
 export async function uploadFile(prevState, formData) {
   const file = formData.get('file')
