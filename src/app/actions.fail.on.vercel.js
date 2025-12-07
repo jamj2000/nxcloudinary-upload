d851947d87fa4456b06085479405906fbe38644a
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-12-07 22:45:58 +0100

    CLOUDINARY_URL

--- src/app/actions.fail.on.vercel.js
+++ src/app/actions.fail.on.vercel.js
@@ -6,11 +6,13 @@ Referencia: https://community.cloudinary.com/discussion/432/image-upload-from-ap
 
 import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+// });
+
+cloudinary.config(process.env.CLOUDINARY_URL || '')
 
 export async function uploadFile(prevState, formData) {
     const file = formData.get('file')
